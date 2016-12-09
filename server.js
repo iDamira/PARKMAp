0d23ee62b8485a6c42e61e558a02d86a3f01744e
Damira Ibragimova <idamira@Damiras-MacBook-Pro.local>
2016-12-09 13:36:29 -0500

    Set up node/express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv').config({ silent: true });
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const PORT = process.argv[2] || process.env.PORT || 3000;
+
+
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(bodyParser.json());
+
+
+
+
+
+
+app.listen(PORT, () => console.log('server here! listening on', PORT));
